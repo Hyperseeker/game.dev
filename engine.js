5617ed9ae45c6d28839112ba332799eb1128c428
Steve Brand <steve@hyperseeker.stream>
2021-03-07 02:20:47 +0700

    Remove obsolete variables

--- engine.js
+++ engine.js
@@ -22,11 +22,6 @@ const SELECTORS = {
 	
 };
 
-const DEFAULT_DESCRIPTION  = "This here is where the description is supposed to be";
-
-const BASE_COMPLEXITY      =   0; // * NOT YET on the 1000 scale
-const BASE_TIME_TO_DEVELOP = 120; // * hours, for 100 complexity, assuming 4hrs/day
-
 const TIMER_RESOLUTION     = 16;
 
 const PLAYER = {
